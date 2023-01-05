b1624609e51898ec7b74531441cac70e6c9349b2
MalcolmKemp <Malokingsley@icloud.com>
2023-01-05 14:24:17 -0500

    "impact sound works"

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let blockMan = document.getElementById('blockMan');
-let block = document.getElementById('block')
+let block = document.getElementById('block');
+let hitSound = new Audio('Audio/explosion.wav')
+let jumpSound = new Audio('Audio/jump.wav')
 function jump(){
     if(blockMan.classList != "animate"){ 
-        let mySound = new Audio('Audio/jump.wav')
-        mySound.play()
+        jumpSound.play()
         
     }
     blockMan.classList.add("animate");
@@ -14,22 +15,27 @@ function jump(){
 }
 
 // Hit detect
-
+function hit(){}
 let checkDead = setInterval(function() {
     let blockManTop =
-    //parseint to get just int(number)
     parseInt(window.getComputedStyle(blockMan).getPropertyValue('top'));
-    let blockleft =
     //parseint to get just int(number)
+    let blockleft =
     parseInt(window.getComputedStyle(block).getPropertyValue('left'));
-    if(blockleft < blockManTop && blockleft == 205 ){
+    //parseint to get just int(number)
+    let blockManRight =
+    parseInt(window.getComputedStyle(blockMan).getPropertyValue('right'));
+    
+    
+    if(blockleft < blockManTop && blockleft == 205 && blockManTop == 575 && blockManRight >= 200 && blockleft != 200){
         // if the block.x is less then blockman.right??
         block.style.animation = "none";
         block.style.display = "none";
-       // console.log(blockManTop, blockleft);
-
-        alert("you lose")
-
+         console.log("Top" + blockManTop,"Right" + blockManRight,"block Left" + blockleft);
+        
+        hitSound.play()
+        alert ( "you lose")
+        
     } else if (blockleft <= 50 && blockManTop >= 40){
     
       // console.log(blockManTop, blockleft)
@@ -47,6 +53,9 @@ let checkDead = setInterval(function() {
 }
 
 
-}},10);
+
+
+
+}},1);
 
 
